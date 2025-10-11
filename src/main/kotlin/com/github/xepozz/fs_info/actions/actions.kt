bd75dfc4ff55257d21d0de4d2ad848645074868e
Dmitriy Derepko <xepozz@list.ru>
2025-10-11 22:12:16 +0400

    fix: run update in a coroutine

--- src/main/kotlin/com/github/xepozz/fs_info/actions/actions.kt
+++ src/main/kotlin/com/github/xepozz/fs_info/actions/actions.kt
@@ -3,17 +3,21 @@ package com.github.xepozz.fs_info.actions
 import com.github.xepozz.fs_info.FileSystemService
 import com.intellij.openapi.actionSystem.ToggleOptionAction
 import com.intellij.openapi.project.DumbAware
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ToggleEnabledAction : DumbAware, ToggleOptionAction({
     object : AbstractToggleAction(it) {
         override val option = settings::enabled
         override fun isEnabled() = true
         override fun setSelected(selected: Boolean) {
+            super.setSelected(selected)
             if (selected) {
-                super.setSelected(true)
-                project
-                    .getService(FileSystemService::class.java)
-                    .refresh()
+                val fileSystemService = project.getService(FileSystemService::class.java)
+                CoroutineScope(Dispatchers.IO).launch {
+                    fileSystemService.refresh()
+                }
             }
         }
     }
